b8c9a16a42f892d3f61e11e728167f229b98ffbb
ariel marte <amas_ariel@discente.ufg.br>
2026-03-11 23:54:58 -0300

    Add Vercel Analytics to React

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { initBotId } from 'botid/client/core'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.tsx'
 
@@ -19,5 +20,6 @@ initBotId({
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
